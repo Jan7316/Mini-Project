5be29ff020c2218b13e4980f9d7180cd19309d84
JanKutos <jckutos@gmail.com>
2016-10-19 15:04:54 +0200

    Made MainMenuActivity fullscreen and in landscape mode

--- app/src/main/java/mini/app/orbis/MainMenuActivity.java
+++ app/src/main/java/mini/app/orbis/MainMenuActivity.java
@@ -1,7 +1,9 @@
 package mini.app.orbis;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -10,4 +12,15 @@ public class MainMenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
     }
+
+    @Override
+    protected  void onResume() {
+        super.onResume();
+
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+    }
+
 }
