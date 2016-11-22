0a64463d803dba65572de04cf9d0456d0728b785
sponkieSkoebuga <sponkie.skoebuga@yahoo.com>
2016-11-22 16:07:26 +0100

    Added VR Rendering System

--- app/src/main/java/mini/app/orbis/VRView.java
+++ app/src/main/java/mini/app/orbis/VRView.java
@@ -9,9 +9,14 @@ import android.opengl.GLSurfaceView;
  */
 
 public class VRView extends GLSurfaceView {
+    VRRenderer renderer;
     public VRView(Context context) {
         super(context);
         setEGLContextClientVersion(2);
+    }
+    public void setRenderer(VRRenderer renderer) {
+        this.renderer = renderer;
+        setRenderer(renderer);
         setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
     }
 }
